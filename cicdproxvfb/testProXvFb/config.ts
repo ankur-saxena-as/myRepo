f612009fc32245dcff2315778b1688e9e1494477
ctmadmin <ctmadmin@L-AC02R5AFNG8WN.local>
2018-02-01 18:28:33 -0800

    added cucumber-junit

--- cicdproxvfb/testProXvFb/config.ts
+++ cicdproxvfb/testProXvFb/config.ts
@@ -1,11 +1,12 @@
 import { browser, Config } from 'protractor';
+import { JsonFormatter } from '../CommonComponents/CucumberReportExtension';
 
 export let config: Config = {
 
     seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
 
     // SELENIUM_PROMISE_MANAGER: true,
-
+ 
     capabilities: {
         browserName: 'chrome',
         chromeOptions: {
@@ -20,6 +21,7 @@ export let config: Config = {
     specs: ["../../testProXvFb/src/typeScript/test/features/*.feature"],
     baseUrl: 'https://www.google.com/',
     resultJsonOutputFile: "./testResults.json",
+
     onPrepare: () => {
         browser.ignoreSynchronization= true;
         browser.manage().timeouts().implicitlyWait(10000);
